8d6f6f377e5089fe38684ea7da79081327da03ea
Justine Charles T. Simbahon <92497072+Jas1028@users.noreply.github.com>
2021-11-09 10:26:17 +0800

    second commit

--- MultipleValues#2.py
+++ MultipleValues#2.py
@@ -12,6 +12,18 @@ def AskHowManyApplesOrangesYouWantToBuy():
     PreferredApple = int(input("An apple cost 20 pesos each. How many apple/s do you prefer to buy? "))
     PreferredOrange = int(input("An orange cost 25 pesos. How many orange/s do you prefer to buy? "))
     return PreferredApple, PreferredOrange
+
+
+def AmountofAppleandOrange(NumberOfAppleAndOrange_, ApplePrice_, OrangePrice_ ):
+    AmountOfApple = int(NumberOfAppleAndOrange_ * ApplePrice_ )
+    AmountOfOrange = int(NumberOfAppleAndOrange_ * OrangePrice_) 
+    print(f"The amount of apple is {AmountOfApple}.")
+    print(f"The amount of orange is {AmountOfOrange}. ")
+
+
 # Steps
 # 1. Ask how many apples and oranges you want to buy
 NumberOfAppleAndOrange = AskHowManyApplesOrangesYouWantToBuy()
+
+# 2. Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
+AmountofAppleandOrange(NumberOfAppleAndOrange, ApplePrice, OrangePrice)
